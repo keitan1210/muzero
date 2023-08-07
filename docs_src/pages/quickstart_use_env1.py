a8801f3534632c6c9dfd72674c87ea59324d0226
恵 <124319373+keitan1210@users.noreply.github.com>
2023-08-07 15:00:38 +0900

    Create quickstart_use_env1.py

---
+++ docs_src/pages/quickstart_use_env1.py
@@ -0,0 +1,10 @@
+import srl
+from srl.algorithms import ql
+
+def main():
+    env_config=srl.EnvConfig("FrozenLake-v1")
+    runner=srl.Runner(env_config,ql.Config())
+    runner.render_terminal()
+
+if __name__=="__main__":
+    main()

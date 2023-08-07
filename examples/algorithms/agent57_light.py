053559364c8f8b5a8167eb6697cea72a04d6ae32
恵 <124319373+keitan1210@users.noreply.github.com>
2023-08-07 15:30:00 +0900

    Create agent57_light.py

---
+++ examples/algorithms/agent57_light.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+import srl
+from srl.algorithms import agent57_light
+from srl.utils import common
+
+common.logger_print()
+
+
+def main():
+    env_config = srl.EnvConfig("Pendulum-v1")
+
+    rl_config = agent57_light.Config(
+        hidden_layer_sizes=(64, 64),
+        enable_rescale=True,
+        #
+        enable_double_dqn=True,
+        enable_dueling_network=True,
+        #
+        actor_num=4,
+        enable_intrinsic_reward=True,
+        input_ext_reward=False,
+        input_int_reward=False,
+        input_action=False,
+    )
+    rl_config.memory.capacity = 100_000
+    rl_config.memory.set_replay_memory()
+    runner = srl.Runner(env_config, rl_config)
+    runner.model_summary()
+
+    # --- train
+    runner.train(max_episodes=200)
+
+    # --- evaluate
+    rewards = runner.evaluate()
+    print(f"Average reward for 20 episodes: {np.mean(rewards)}")
+
+
+if __name__ == "__main__":
+    main()

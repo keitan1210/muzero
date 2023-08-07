57fd76c0459705de2bfff31caab6264596fab1a4
恵 <124319373+keitan1210@users.noreply.github.com>
2023-08-07 15:34:42 +0900

    Create r2d2.py

---
+++ examples/algorithms/r2d2.py
@@ -0,0 +1,57 @@
+import os
+
+import matplotlib.pyplot as plt
+
+import srl
+from srl.algorithms import r2d2
+
+
+def main():
+    env_config = srl.EnvConfig("Pendulum-v1")
+    rl_configs = []
+
+    # ハイパーパラメータ
+    r2d2_base = r2d2.Config(
+        lstm_units=64,
+        hidden_layer_sizes=(64,),
+        enable_dueling_network=False,
+        target_model_update_interval=100,
+        enable_rescale=False,
+        burnin=5,
+        sequence_length=5,
+        enable_retrace=False,
+    )
+    r2d2_base.memory.capacity = 100_000
+
+    # no retrace
+    rl_configs.append(("no retrace", r2d2_base.copy()))
+
+    # retrace
+    _c = r2d2_base.copy()
+    _c.enable_retrace = True
+    rl_configs.append(("retrace", _c))
+
+    # train
+    results = []
+    for name, rl_config in rl_configs:
+        print(name)
+        runner = srl.Runner(env_config, rl_config)
+        runner.set_history()
+        runner.train(max_episodes=50)
+        results.append((name, runner.get_history().get_df()))
+
+    # plot
+    plt.figure(figsize=(8, 4))
+    plt.xlabel("episode")
+    plt.ylabel("reward")
+    for name, df in results:
+        plt.plot(df["actor0_episode"], df["actor0_eval_reward0"].rolling(10).mean(), label=name)
+    plt.grid()
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(os.path.join(os.path.dirname(__file__), "_r2d2.png"))
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()

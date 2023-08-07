3151610fe66c5bfb0fc90bc4b5bd3fd97ea5ff53
恵 <124319373+keitan1210@users.noreply.github.com>
2023-08-07 14:01:22 +0900

    Create custom_algorithm1.py

---
+++ docs_src/pages/custom_algorithm1.py
@@ -0,0 +1,11 @@
+from srl.rl.memories.sequence_memory import SequenceRemoteMemory
+
+class MyRemoteMemory(SequenceRemoteMemory):
+    pass
+
+memory=MyRemoteMemory(None)
+memory.add([1,2])
+memory.add([2,3])
+memory.add([3,4])
+dat=memory.sample()
+print(dat)

bd2cf13d2a3f101754a5db27eae55329c3733e10
恵 <124319373+keitan1210@users.noreply.github.com>
2023-08-07 14:44:00 +0900

    Create custom_env1.py

---
+++ docs_src/pages/custom_env1.py
@@ -0,0 +1,14 @@
+import srl
+from srl.envs import sample_env
+env=srl.make_env("SampleEnv")
+state=env.reset(render_mode="terminal")
+total_reward=0
+env.render()
+
+while not env.done:
+    action=env.sample_action()
+    env.step(action)
+    total_reward+=env.reward
+    print(f"step{env.step_num},action{action},reward{env.reward},done{env.done}")
+    env.render()
+print(total_reward)

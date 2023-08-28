dbd6471b04ccc0ed29b1b8eae00bdbff02d1872d
恵 <124319373+keitan1210@users.noreply.github.com>
2023-08-28 09:18:44 +0900

    Create singleplay.py

---
+++ srl/base/env/genre/singleplay.py
@@ -0,0 +1,28 @@
+from abc import abstractmethod
+from typing import List,Tuple
+
+from srl.base.define import EnvActionType,EnvObservationType,InfoType,InvalidActionType
+from srl.base.env import EnvBase
+
+class SinglePlayEnv(EnvBase):
+    @abstractmethod
+    def call_reset(self) -> Tuple[EnvObservationType,InfoType]:
+        raise NotImplementedError()
+    @abstractmethod
+    def call_step(self,action:EnvActionType) -> Tuple[EnvObservationType,float,bool,InfoType]:
+        raise NotImplementedError()
+    def call_get_invalid_actions(self) -> InvalidActionType:
+        return []
+    @property
+    def player_num(self) -> int:
+        return 1
+    @property
+    def next_player_index(self) -> int:
+        return 0
+    def reset(self) -> Tuple[EnvObservationType,InfoType]:
+        return self.call_reset()
+    def step(self,action:EnvActionType) -> Tuple[EnvObservationType,List[float],bool,InfoType]:
+        n_state,reward,done,info = self.call_step(action)
+        return n_state,[reward],done,info
+    def get_invalid_actions(self,player_index:int) -> InvalidActionType:
+        return self.call_get_invalid_actions()
